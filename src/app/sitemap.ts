040bc156111e455a5d4844cd3bcf8f9c929c54dd
Ratzifutzi <joshua@hyper-tech.ch>
2026-01-29 16:00:39 +0100

    Added PrettierRC

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,12 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from 'next';
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://example.com/',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1.0,
-    },
-  ]
+	return [
+		{
+			url: 'https://example.com/',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1.0,
+		},
+	];
 }
